abf62e63fd3f2db041eab3d8d6a97776cb00a982
Affan Sayeed <affansayeed234@gmail.com>
2025-07-29 14:49:30 +0530

    Add prod uri

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: "https://rideonix-backend.onrender.com/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       // send data to db here
