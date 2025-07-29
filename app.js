a00da845cb33f8ed6a220f316197b293e490dd93
Affan Sayeed <affansayeed234@gmail.com>
2025-07-29 14:59:57 +0530

    Add prod uri

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const rateLimit = require("express-rate-limit");
 const passport = require("passport");
 const session = require("express-session");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
